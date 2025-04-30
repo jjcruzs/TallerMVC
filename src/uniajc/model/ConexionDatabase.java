4245f45c623b0cfd210e24d68cf0e37d6a61cb1a
Gabriel Perez <gperezm@profesores.uniajc.edu.co>
2025-04-29 19:48:12 -0500

    Creacion de archivo properties para proteger los datos de conexion a la base de datos

--- src/uniajc/model/ConexionDatabase.java
+++ src/uniajc/model/ConexionDatabase.java
@@ -4,9 +4,12 @@
  */
 package uniajc.model;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 /**
  *
@@ -19,14 +22,19 @@ public class ConexionDatabase {
     public static Connection getConnection() {
         if (conexion == null) {      
             try {
-                String url = "jdbc:mysql://localhost:3306/escuela";
-                String user = "root";
-                String password = "admin";
+                
+                Properties props = new Properties();
+                
+                props.load(new FileInputStream("db.properties"));
+                
+                String url = props.getProperty("db.url");
+                String user = props.getProperty("db.user");
+                String password = props.getProperty("db.password");
 
                 conexion = DriverManager.getConnection(url, user, password);
                 System.out.println("Conexion exitosa!");
                 System.out.println("");
-            } catch (SQLException error){
+            } catch (SQLException | IOException error){
                 System.out.println("Error conectando a la base de datos: " + error);
             }   
         }
